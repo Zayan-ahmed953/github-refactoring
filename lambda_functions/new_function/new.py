cd761c2cc2421e636ce9743c2d3150dde5b4e16d
Zayan Ahmed <zayan.ahmad384@gmail.com>
2026-03-06 12:32:59 +0500

    Update response message in lambda_handler

--- lambda_functions/new_function/new.py
+++ lambda_functions/new_function/new.py
@@ -4,5 +4,6 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from v22!')
+        'body': json.dumps('Hello from new function!')
     }
+
